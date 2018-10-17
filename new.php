24a9c006898bcde68de915227e1cd8be868719eb
Praful kumar sharma <praful.aipl01@gmail.com>
2018-10-17 17:51:57 +0530

    4 line change in new.php

--- new.php
+++ new.php
@@ -2,10 +2,6 @@
 
 echo 'that is new file';
 echo 'this is new file';
-echo 'this is new file';
-echo 'this is new file';
-echo 'this is new file';
-echo 'this is new file';
-echo 'this is new file';
+
 
 ?>
